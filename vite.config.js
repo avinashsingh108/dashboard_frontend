58fcdea130e4f1e2fed14b1f766954b6b36ee23d
avinashsingh108 <TsaPh@69@HMS108>
2025-02-03 23:58:40 +0530

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server:{
-    proxy: {
-      '/api': {
-        target: 'http://3.111.196.92:8020',
-        changeOrigin: true,  // Adjusts the origin of the request
-        secure: false,       // Disables SSL verification (not needed for HTTP)
-      },
-    },
-  }
 });
